10b2469ab05aa997b72c774ea80189b9994b9027
vargantianuradha <46062869+vargantianuradha@users.noreply.github.com>
2020-11-20 17:19:08 +0530

    66

--- modules/sample/src/main/java/sample/portlet/SamplePortlet.java
+++ modules/sample/src/main/java/sample/portlet/SamplePortlet.java
@@ -34,7 +34,7 @@ public class SamplePortlet extends MVCPortlet {
 	@Override
 	public void doView(RenderRequest renderRequest, RenderResponse renderResponse)
 			throws IOException, PortletException {
-	System.out.println("nexustest55:::::::::::::");
+	System.out.println("nexustest66:::::::::::::");
 		super.doView(renderRequest, renderResponse);
 	}
 }
