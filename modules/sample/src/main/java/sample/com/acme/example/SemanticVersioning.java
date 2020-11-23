e3c9d87eb82fbd360c5bb137eaad3f23e6daa9fb
root <root@ip-172-31-44-76.ap-south-1.compute.internal>
2020-11-23 08:00:36 +0000

    added

---
+++ modules/sample/src/main/java/sample/com/acme/example/SemanticVersioning.java
@@ -0,0 +1,19 @@
+package com.acme.example;
+
+public final class SemanticVersioning {
+
+  //tag::anchor[]
+
+  /**
+   * A sample for the javadoc
+   *
+   * @param args Tha arguments to this magnificient application
+   * @since 0.0.1
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello Semantic versioning");
+  }
+
+  //end::anchor[]
+
+}
